4f7b72a69480b016ed25de6bda332c3f26e34682
Anthony Gutierrez <adgutierrez@techbyte.net>
2019-06-10 17:44:51 -0700

    Redo Stadium and Playing Field. Remove dups and errors. Save as Assemblies

---
+++ Assets/SeatingAssembly/FuelScoring.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuelScoring : MonoBehaviour
+{
+    public GameObject effectObject;
+    public Text scoreTotalText;
+    public int scoreTotalValue;
+    private int score;
+
+    // Use this for initialization
+    void Start ()
+    {
+        effectObject.SetActive(false);
+        score = 0;
+        UpdateScore();
+    }
+
+    private void OnTriggerEnter(Collider otherCollider)
+    {
+        if(otherCollider.GetComponent<Collider>() != null)
+        {
+            effectObject.SetActive(true);
+            AddScore(scoreTotalValue);
+            
+            Debug.Log("matched collider requiredments");
+        }
+    }
+
+    void Update () {
+		
+	}
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreTotalText.text = score.ToString();
+        Debug.Log("Updated Fuel Score");
+    }
+}
---
+++ Assets/SeatingAssembly/GearScore.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GearScore : MonoBehaviour
+{
+    //private GameController gameController;
+
+    public Text scoreGearTotalText;
+    public int scoreGearTotalValue;
+    private int score;
+
+    // Use this for initialization
+    void Start ()
+    {
+        score = 0;
+        UpdateScore();
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "FieldCollider" || other.tag == "Player")
+        {
+            return;
+        }
+
+        if (other.gameObject.CompareTag("GearPickup"))
+        {
+            other.gameObject.SetActive(false);
+            AddScore(scoreGearTotalValue);
+            Debug.Log("Gear Pickup Tag" + other);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreGearTotalText.text = score.ToString();
+        Debug.Log("Updated Fuel Score");
+    }
+}
